a9ab6d4193e4a956262b090982b141c6ee04fd7b
PUNpun1407 <p.punn@hotmail.com>
2020-05-09 20:52:57 +0700

    call api with token

--- PM2HAHA/PM2HAHA/Models/Class.cs
+++ PM2HAHA/PM2HAHA/Models/Class.cs
@@ -8,36 +8,35 @@ namespace PM2HAHA.Models
     public class Products
     {
    
-        public string client_ip { get; set; }
-        public string abbreviation { get; set; }
-        public DateTime datetime { get; set; }
-        public int day_of_week { get; set; }
-        public int day_of_year { get; set; }
-        public bool dst { get; set; }
-
-        public int? dst_from { get; set; }
-        public int dst_offset { get; set; }
-        public int? dst_until { get; set; }
-        public int raw_offset { get; set; }
-
-        public string timezone { get; set; }
-        public int unixtime { get; set; }
-        public DateTime utc_datetime { get; set; }
-
-        public string utc_offset { get; set; }
-
-        public int week_number { get; set; }
-        public string[] Sizes { get; set; }
-        public string Name { get; set; }
-        public DateTime Expiry { get; set; }
+        public string status { get; set; }
+        public Data data { get; set; }
+        
 
 
+        public string s { get; set; }
+        public string tz { get; set; }
 
+       
+        public int pm25 { get; set; }
+        public int url { get; set; }
 
-
-
-
+        //{"status":"ok","data":{"aqi":95,"idx":5775,"attributions":[{"url":"http://aqmthai.com/","name":"Division of Air Quality Data, Air Quality and Noise Management Bureau, Pollution Control Department.","logo":"Thailand-PCD.png"},{"url":"http://worldweather.wmo.int","name":"World Meteorological Organization - surface synoptic observations (WMO-SYNOP)"},{"url":"https://waqi.info/","name":"World Air Quality Index Project"}],"city":{"geo":[18.787747,98.9931284],"name":"Chiang Mai","url":"https://aqicn.org/city/chiang-mai"},"dominentpol":"pm25","iaqi":{"co":{"v":0.1},"h":{"v":40},"no2":{"v":2.4},"o3":{"v":16.6},"p":{"v":1005.7},"pm10":{"v":40},"pm25":{"v":95},"so2":{"v":0.6},"t":{"v":40.8},"w":{"v":1.3}},"time":{"s":"2020-05-09 15:00:00","tz":"+07:00","v":1589036400},"debug":{"sync":"2020-05-09T18:09:28+09:00"}}}
 
 
     }
+    public class Data
+    {
+        public int idx { get; set; }
+        public int aqi { get; set; }
+        public City city { get; set; }
+
+    }
+    public class City 
+    {
+        public ArrayList(int capacity);
+        public virtual decimal geo { get; set; }
+        
+        public string name { get; set; }
+    }
+
 }
